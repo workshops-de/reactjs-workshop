30bbe3bf5fbe63698aaf69aa79b752ec5a06d893
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2018-12-04 10:53:27 +0100

    Exercise 2 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import NameRaw from './components/NameRaw';
+import Name from './components/Name';
 import './App.css';
 
 class App extends Component {
   render() {
-    return React.createElement(NameRaw, null, null);
+    return (
+      <div>
+        <Name />
+        <NameRaw />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Name.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Name = () => {
+  const name = 'Max Mustermann';
+  return <div>Hello {name}</div>;
+};
+
+export default Name;
