b18a699f239d060f4464f705c5b27b8baf6a2f60
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2018-12-14 21:43:20 +0100

    Refactor reducer actions and constants

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,40 +6,78 @@ export function addDummyBook() {
   return { type: types.ADD_DUMMY };
 }
 
-function requestBooks() {
-  return { type: types.REQUEST_BOOKS };
+export function updateBook(book) {
+  return { type: types.UPDATE_BOOK, book };
 }
 
-function requestBook() {
-  return { type: types.REQUEST_BOOK };
-}
-
-function receiveBooks(books) {
-  return { type: types.RECEIVE_BOOKS, books };
-}
-
-function receiveBook(book) {
-  return { type: types.RECEIVE_BOOK, book };
-}
-
+// fetchBooks
+function fetchBooksPending() { return { type: types.FETCH_BOOKS_PENDING } }
+function fetchBooksSuccess(books) { return { type: types.FETCH_BOOKS_SUCCESS, books } }
+function fetchBooksError(error) { return { type: types.FETCH_BOOKS_ERROR, error } }
 export function fetchBooks() {
   return dispatch => {
 
-    dispatch(requestBooks());
+    dispatch(fetchBooksPending());
 
     return fetch(BOOKS_URL)
       .then(response => response.json())
-      .then(books => dispatch(receiveBooks(books)));
+      .then(books => dispatch(fetchBooksSuccess(books)))
+      .catch(error => dispatch(fetchBooksError(error)));
   }
 }
 
-export function fetchBook(id) {
+// fetchBookDetails
+function fetchBookDetailsPending() { return { type: types.FETCH_BOOK_DETAILS_PENDING } }
+function fetchBookDetailsSuccess(book) { return { type: types.FETCH_BOOK_DETAILS_SUCCESS, bookDetails: book } }
+export function fetchBookDetails(isbn) {
   return dispatch => {
 
-    dispatch(requestBook());
+    dispatch(fetchBookDetailsPending());
 
-    return fetch(`${BOOKS_URL}/${id}`)
+    return fetch(`${BOOKS_URL}/${isbn}`)
       .then(response => response.json())
-      .then(book => dispatch(receiveBook(book)));
+      .then(book => dispatch(fetchBookDetailsSuccess(book)));
+  }
+}
+
+// fetchBookEdit
+function fetchBookEditPending() { return { type: types.FETCH_BOOK_EDIT_PENDING } }
+function fetchBookEditSuccess(book) { return { type: types.FETCH_BOOK_EDIT_SUCCESS, book } }
+function fetchBookEditError(error) { return { type: types.FETCH_BOOK_EDIT_ERROR, error } }
+export function fetchBookEdit(isbn) {
+  return dispatch => {
+
+    dispatch(fetchBookEditPending());
+
+    return fetch(`${BOOKS_URL}/${isbn}`)
+      .then(response => response.json())
+      .then(book => dispatch(fetchBookEditSuccess(book)))
+      .catch((error) => dispatch(fetchBookEditError(error)));
+  }
+}
+
+// persistBook
+function persistBookPending() { return { type: types.PERSIST_BOOK_PENDING } }
+function persistBookSuccess(book) { return { type: types.PERSIST_BOOK_SUCCESS, book } }
+function persistBookError(error) { return { type: types.PERSIST_BOOK_ERROR, error } }
+export function persistBook(book) {
+  return dispatch => {
+
+    dispatch(persistBookPending());
+
+    const request = new Request(`${BOOKS_URL}/${book.isbn}`, {
+      headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        method : 'PUT',
+        body   : JSON.stringify(book)
+      }
+    );
+
+    return fetch(request)
+      .then(response => response.json())
+      .then((book) => dispatch(persistBookSuccess(book)))
+      .catch((error) => dispatch(persistBookError(error)));
   }
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,40 +2,149 @@ import types from './constants';
 
 const INITIAL_STATE = {
   books : [],
-  bookDetail: null,
-  loading: false,
-  loadingDetail: false
+  bookDetails: null,
+  bookEdit: null,
+  loading: {},
+  errors: {}
 };
 
 export default function bookMonkeyApp (state = INITIAL_STATE, action) {
   switch (action.type) {
+
     case types.ADD_DUMMY:
       return {
         ...state,
         books: state.books.concat({title: 'DUMMY BOOK'})
       };
-    case types.REQUEST_BOOKS:
+    case types.UPDATE_BOOK:
       return {
         ...state,
-        loading: true
+        bookEdit: action.book
+      };
+
+    // fetchBooks
+    case types.FETCH_BOOKS_PENDING:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          books: true
+        }
       }
-    case types.RECEIVE_BOOKS:
+    case types.FETCH_BOOKS_SUCCESS:
       return {
         ...state,
         books: action.books,
-        loading: false
+        loading: {
+          ...state.loading,
+          books: false
+        }
       }
-    case types.REQUEST_BOOK:
+    case types.FETCH_BOOKS_ERROR:
       return {
         ...state,
-        loadingDetail: true
+        loading: {
+          ...state.loading,
+          books: false
+        },
+        errors: {
+          ...state.error,
+          books: action.error
+        }
       }
-    case types.RECEIVE_BOOK:
+
+    // fetchBookDetails
+    case types.FETCH_BOOK_DETAILS_PENDING:
       return {
         ...state,
-        bookDetail: action.book,
-        loadingDetail: false
+        bookDetails: null,
+        loading: {
+          ...state.loading,
+          bookDetails: true
+        }
       }
+    case types.FETCH_BOOK_DETAILS_SUCCESS:
+      return {
+        ...state,
+        bookDetails: action.bookDetails,
+        loading: {
+          ...state.loading,
+          bookDetails: false
+        }
+      }
+    case types.FETCH_BOOK_DETAILS_ERROR:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          bookDetails: false
+        },
+        errors: {
+          ...state.error,
+          bookDetails: action.error
+        }
+      }
+
+    // fetchBookEdit
+    case types.FETCH_BOOK_EDIT_PENDING:
+      return {
+        ...state,
+        bookEdit: null,
+        loading: {
+          ...state.loading,
+          bookEdit: true
+        }
+      }
+    case types.FETCH_BOOK_EDIT_SUCCESS:
+      return {
+        ...state,
+        bookEdit: action.book,
+        loading: {
+          ...state.loading,
+          bookEdit: false
+        }
+      }
+    case types.FETCH_BOOK_EDIT_ERROR:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          bookEdit: false
+        },
+        errors: {
+          ...state.error,
+          bookEdit: action.error
+        }
+      }
+
+    // persistBook
+    case types.PERSIST_BOOK_PENDING:
+      return {
+        ...state,
+        loading: {
+          ...state.loading,
+          persistBook: true
+        }
+      }
+    case types.PERSIST_BOOK_SUCCESS:
+      return {
+        ...state,
+        bookEdit: action.book,
+        loading: {
+          ...state.loading,
+          persistBook: false
+        }
+      }
+    case types.PERSIST_BOOK_ERROR:
+      return {
+        ...state,
+        errors: {
+          ...state.error,
+          persistBook: action.error
+        }
+      }
+
+
     default:
       return state;
   }
